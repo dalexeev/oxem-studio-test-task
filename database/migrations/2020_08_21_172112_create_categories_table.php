f5b1050b3bc0d3e6b6839607a6e17869b11230b4
Данил Алексеев <danil@alexeev.xyz>
2020-08-21 20:46:34 +0300

    Добавлены миграции

---
+++ database/migrations/2020_08_21_172112_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->string('external_id', 200)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
